2174ec1207952ef1a78be95846d4305e8474c231
Iago Marinheiro Elias <go.marinheiro@gmail.com>
2020-03-02 09:34:56 -0300

    change name function

--- rsa/util.py
+++ rsa/util.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-def is_relative_prime(a, b):
+def is_relatively_prime(a, b):
     """Retorna ``True`` se ``a`` e ``b`` forem relativamente numeros primos.
     Dois numeros são relativamente primos se não compatilharem fatores comuns,
     ou seja, nao ha um numero inteiro (exceto 1) que divida ambos os numeros.
